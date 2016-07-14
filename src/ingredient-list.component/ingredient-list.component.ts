d8c83f1e35048e46bdbb13d807d0aae2a5e07541
Michael Hanley <mhanley@sacfoodcoop.com>
2016-07-14 10:37:14 -0700

    new forms for the Ingredient-list component

--- src/ingredient-list.component/ingredient-list.component.ts
+++ src/ingredient-list.component/ingredient-list.component.ts
@@ -2,23 +2,25 @@ import {Component} from '@angular/core';
 import {OnInit} from '@angular/core';
 import {ShoppingListService} from "../services/shopping-list.service";
 import {Ingredient} from "../shared/ingredient";
-import {ControlGroup} from "@angular/common";
-import {FormBuilder} from "@angular/common";
-import {Validators} from "@angular/common";
-import {Control} from "@angular/common";
+import {FormGroup, FormControl, REACTIVE_FORM_DIRECTIVES, Validators} from "@angular/forms";
 
 @Component
 ({
     selector: 'ingredient-list',
+	directives: [REACTIVE_FORM_DIRECTIVES],
     templateUrl: "src/ingredient-list.component/ingredient-list.tpl.html",
 })
 export class IngredientListComponent implements OnInit{
 	ingredientList: Ingredient[];
 	tableFlag:boolean = false;
-	item = {name: '', amount: 0};
-	myForm: ControlGroup;
+	myForm: FormGroup;
 
-	constructor(private _shoppingListService: ShoppingListService, private _formBuilder: FormBuilder) { }
+	constructor(private _shoppingListService: ShoppingListService) {
+		this.myForm = new FormGroup({
+			'itemName': new FormControl('', Validators.required),
+			'amount': new FormControl('', [Validators.required, Validators.pattern("^[0-9]*$")])
+		});
+	 }
 
 	onDeleteIngredient(index:number)
 	{
@@ -42,25 +44,12 @@ export class IngredientListComponent implements OnInit{
 
 	onSubmit()
 	{
-		let k:number = this.item.amount;
-		this._shoppingListService.addItem({Ingredient: this.item.name, ingredientCount: k});
+		console.log(this.myForm.value.itemName);
+		let k:number = this.myForm.value.amount;
+		this._shoppingListService.addItem({Ingredient: this.myForm.value.itemName, ingredientCount: k});
 	}
 
 	ngOnInit():any {
 		this.ingredientList = this._shoppingListService.getIngredientList();
-
-		this.myForm = this._formBuilder.group({ 
-			'ingredient': ['',Validators.required],
-			'ingredientCount': ['', Validators.compose([
-				Validators.required,
-				greaterZero
-				])],
-		});
-	}
-}
-
-function greaterZero(control: Control): {[s: string]: boolean} {
-	if (control.value <= 0) {
-		return {notEnough: true};
 	}
 }
